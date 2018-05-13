8f95a0461bb9bf10a1e26a57de4c160c9146d0ce
manzen <ririn.eva0129@gmail.com>
2018-05-13 18:18:12 +0900

    Myページを実装

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -1,4 +1,8 @@
 class Interest < ApplicationRecord
   belongs_to :alcohol
   belongs_to :user
+
+  def self.search_by_interest(user_id)
+    Interest.where(user_id: user_id)
+  end
 end
--- app/models/recommend.rb
+++ app/models/recommend.rb
@@ -1,4 +1,8 @@
 class Recommend < ApplicationRecord
   belongs_to :alcohol
   belongs_to :user
+
+  def self.search_by_recommend(user_id)
+    Recommend.where(user_id: user_id)
+  end
 end
