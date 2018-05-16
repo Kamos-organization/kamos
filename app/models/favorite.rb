dfe5d96dbdfe884f22e0b2f9cb418099b70d7ba6
manzen <ririn.eva0129@gmail.com>
2018-05-17 00:48:47 +0900

    修正漏れ対応

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -6,7 +6,7 @@ class Favorite < ApplicationRecord
     Favorite.where(user_id: user_id)
   end
 
-  def self.is_exist?(alchol_id, user_id)
-    Favorite.where(alchol_id, user_id)
+  def self.is_exist?(alcohol_id, user_id)
+    Favorite.where(alcohol_id: alcohol_id, user_id: user_id).first
   end
 end
